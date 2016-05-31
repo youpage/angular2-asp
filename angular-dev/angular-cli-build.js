664633ae887916487b38f787d78afb22c6a02439
YouPage <cgarbacea@gmail.com>
2016-05-31 17:48:49 +0300

    .map are requested behind the scene

--- angular-dev/angular-cli-build.js
+++ angular-dev/angular-cli-build.js
@@ -2,16 +2,23 @@
 
 var Angular2App = require('angular-cli/lib/broccoli/angular2-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   return new Angular2App(defaults, {
     vendorNpmFiles: [
       'systemjs/dist/system-polyfills.js',
+      'systemjs/dist/system-polyfills.map',
       'systemjs/dist/system.src.js',
+      'systemjs/dist/system.src.map',
       'zone.js/dist/*.js',
+      'zone.js/dist/*.map',
       'es6-shim/es6-shim.js',
+      'es6-shim/es6-shim.map',
       'reflect-metadata/*.js',
+      'reflect-metadata/*.map',
       'rxjs/**/*.js',
-      '@angular/**/*.js'
+      'rxjs/**/*.map',
+      '@angular/**/*.js',
+      '@angular/**/*.map'
     ]
   });
 };
