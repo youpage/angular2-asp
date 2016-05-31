12600e08217737307356a1b474ec652daaa204f9
YouPage <cgarbacea@gmail.com>
2016-05-31 17:58:39 +0300

    app layout component

--- angular-dev/src/app/angular-dev.component.ts
+++ angular-dev/src/app/angular-dev.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES, Routes, Router } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerComponent } from './+customer/customer.component';
+import { APP_PROVIDERS } from './app.providers';
 
 @Component({
-  moduleId: module.id,
-  selector: 'angular-dev-app',
-  templateUrl: 'angular-dev.component.html',
-  styleUrls: ['angular-dev.component.css']
+    moduleId: module.id, //Needed to be able to resolve relative urls for templates and styles.
+    selector: 'angular-dev-app',
+    template: `<router-outlet></router-outlet>`,
+    directives: [ROUTER_DIRECTIVES],
+    providers: [APP_PROVIDERS]
 })
+@Routes([
+    { path: '/', component: DashboardComponent },
+    { path: '/customer/:id', component: CustomerComponent },
+    { path: '*', component: DashboardComponent }
+])
 export class AngularDevAppComponent {
-  title = 'angular-dev works!';
+  
+    title = 'angular-dev works!';
+    constructor(private router: Router) {
+
+    }
+
 }
