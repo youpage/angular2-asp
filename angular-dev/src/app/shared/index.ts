6747cd79b498017354c320fce262c5a52b11185b
YouPage <cgarbacea@gmail.com>
2016-05-31 17:54:42 +0300

    exports

---
+++ angular-dev/src/app/shared/index.ts
@@ -0,0 +1,19 @@
+//Components
+export {FilterTextboxComponent} from './components/filterTextbox.component';
+
+//Directives
+export {SortByDirective} from './directives/sortby.directive';
+
+//Pipes
+export {CapitalizePipe} from './pipes/capitalize.pipe';
+export {TrimPipe} from './pipes/trim.pipe';
+
+//Services
+export {DataService} from './services/data.service';
+export {TrackByService} from './services/trackby.service';
+
+//Utils
+export {Sorter} from './utils/sorter';
+export {HttpUtils} from './utils/httpUtils';
+
+export * from './interfaces';
